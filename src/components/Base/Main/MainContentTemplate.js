533dffbb528b1e46c1719e7b954e3e150bb36382
hy_sean <comet5485@gmail.com>
2019-03-29 02:20:38 +0900

    Add sematic ui react

--- src/components/Base/Main/MainContentTemplate.js
+++ src/components/Base/Main/MainContentTemplate.js
@@ -1,5 +1,13 @@
+import _ from 'lodash'
 import React, { Component } from "react";
+import { Grid, Segment } from 'semantic-ui-react'
 import { Trending, Recent, Tag } from 'components/bbs';
+
+const GridColumns = _.times(3, i => (
+  <Grid.Column key={i}>
+    <Trending />
+  </Grid.Column>
+));
 
 class MainContentTemplate extends Component {
 
@@ -10,10 +18,14 @@ class MainContentTemplate extends Component {
     switch (mode) {
       case 'recent':
         return <Recent />
-      case 'tag':
+      case 'tags':
         return <Tag />
       default:
-        return <Trending />
+        return (
+          <Grid container columns={3}>
+            { GridColumns }
+          </Grid>
+        )
     }
   }
 }
